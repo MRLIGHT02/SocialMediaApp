ecbddde70d6c10d7e955a1bfdb086547c1f21bf7
Saif Ali <saifali958251034@gmail.com>
2025-04-21 12:41:30 +0530

    Changed Entery Dynamic

---
+++ Controls/NullBorderClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace SocialMediaApp.Controls
+{
+    public class NullBorderClass:Entry
+    {
+
+        protected override void OnHandlerChanged()
+            {
+            base.OnHandlerChanged();
+            SetBorderLessBackground();
+            }
+        private void SetBorderLessBackground()
+            {
+             
+            }
+    }
+}
