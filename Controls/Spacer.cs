151c7a7309ea1494bed731114a5688229b6f9a1e
Saif Ali <saifali958251034@gmail.com>
2025-04-21 13:30:58 +0530

    changed spacer

--- Controls/Spacer.cs
+++ Controls/Spacer.cs
@@ -1,36 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SocialMediaApp.Controls
+﻿public class Spacer : ContentView
     {
-    public class Spacer:ContentView
+    public BoxView Box { get; set; }
+
+    public Spacer()
         {
-        public BoxView Box{ get; set; }
-        public Spacer()
+        Box = new BoxView
             {
-            Box = new BoxView
-                {
-                BackgroundColor = Colors.Transparent,
-                HeightRequest = 20
+            BackgroundColor = Colors.Transparent,
+            HeightRequest = 20
+            };
 
-                };
+        Content = Box;
+        SetValue(SpaceProperty, 20); // optional but good
+        }
 
-            }
-        public static readonly BindableProperty SpaceProperty = BindableProperty.Create(nameof(Space), typeof(int), typeof(Spacer), defaultValue: 20, propertyChanged: OnSpacePropertyChanged);
+    public static readonly BindableProperty SpaceProperty =
+        BindableProperty.Create(
+            nameof(Space),
+            typeof(int),
+            typeof(Spacer),
+            defaultValue: 20,
+            propertyChanged: OnSpacePropertyChanged);
 
-        public int Space { 
-            get=>(int)GetValue(SpaceProperty); 
-            set=>SetValue(SpaceProperty,value); 
-            }
-        private static void OnSpacePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+    public int Space
+        {
+        get => (int)GetValue(SpaceProperty);
+        set => SetValue(SpaceProperty, value);
+        }
+
+    private static void OnSpacePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+        if (bindable is Spacer spacer)
             {
-           if(bindable is Spacer spacer)
-                {
-                spacer.Box.HeightRequest = spacer.Space;
-                }
+            spacer.Box.HeightRequest = spacer.Space;
             }
         }
     }
